ab313f7d39148bb1485bac9c2e96b523c85bc9d2
m70 <m70@m70-VB>
2017-10-20 17:55:33 +0200

    Versió 0.7-C del projecte prjava43ramon18. Bramca master Modificada.

--- src/prjava43ramon18/Prjava43ramon18.java
+++ src/prjava43ramon18/Prjava43ramon18.java
@@ -2,6 +2,7 @@
 package prjava43ramon18;
 import java.awt.*;        // using AWT containers and components
 import java.awt.event.*;  // using AWT events and listener interfaces
+import java.sql.*;
 import java.io.*;
 // An AWT GUI program inherits the top-level container java.awt.Frame
 public class Prjava43ramon18 extends Frame
